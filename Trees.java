d0f729ceb062b7ae3e7b81eee3e50a574f205de0
blakefurlano <58089528+blakefurlano@users.noreply.github.com>
2019-11-26 11:45:58 -0600

    Add files via upload

--- Trees.java
+++ Trees.java
@@ -67,40 +67,68 @@ public class Trees {
         BinarySearchTree<Place> b = new BinarySearchTree<Place>();
         AVLTree<Place> a = new AVLTree<Place>();
 */
-        String lastName;
-        int lastCode;
 
-        String firstLine = data.nextLine();
+
+        String firstLine = data.nextLine();//gets ignored
+        //grabs first city data
+        /*
         int zipCode = data.nextInt();
         double xValue = data.nextDouble(); //ignore
         double yValue = data.nextDouble(); //ignore
         String cityState = data.nextLine().trim();
-        //System.out.println("HERE");
-        Place newPlace = new Place(cityState, zipCode);
-        //System.out.println("Past");
+
+
+        Place newPlace = new Place(cityState, zipCode);//creates a place for this city data
+        //inserts this place into the tree
+        s.insert(newPlace);
+        b.insert(newPlace);
+        a.insert(newPlace);
         lastName = cityState.trim();
 
+         */
+        String prevName = " ";//used for comparisons between lines
+        Place holdPlace = new Place("Hold", 5); //dummy temp place that gets overwritten
         while (data.hasNextLine()) {
+            //get the data for a city
             int zipCode1 = data.nextInt();
             double  xValue1= data.nextDouble(); //ignore
             double yValue1 = data.nextDouble(); //ignore
             String cityState1 = data.nextLine().trim();
 
-            if (cityState1.equals(lastName)) {
-                //System.out.println("HERE");
-                //System.out.println(zipCode1);
-                newPlace.addZip(zipCode1);//problem
-                //System.out.println("HERE2");//add zip code to existing city
+
+           // Place holdPlace = new Place("Hold", 5);
+            //System.out.println(zipCode1 + " " + xValue1 + " " + yValue1 + " " +cityState1);
+            if(prevName.equalsIgnoreCase(" ")){//the first town to insert
+                Place newPlace2 = new Place(cityState1,zipCode1);//creates a place for first line
+                s.insert(newPlace2);
+                b.insert(newPlace2);
+                a.insert(newPlace2);
+                holdPlace = newPlace2;//sets the temp place = the first line
+            }
+            else {
+                if (cityState1.equals(prevName)) {
+                    //adds the zip to the place that is in hold because we know that the line we are currently
+                    //on has the same city state as the previous line.
+                    //do not need to do hold = newPlace2 since we never created a new place
+                    System.out.println("Here");
+                    holdPlace.addZip(zipCode1);//add zip code to existing city
+                    //System.out.println("HERE2");
+                /*
                 if (!data.hasNextLine()) {                  // Last line of file check
-                    s.insert(newPlace);                 //delete the printstream
+                    s.insert(newPlace);
                     b.insert(newPlace);
                     a.insert(newPlace);
                 }
-            } else {
-                s.insert(newPlace);                 //delete the printstream
-                b.insert(newPlace);
-                a.insert(newPlace);
-
+                 */
+                } else {//the line we are on does not match the previous lines city state
+                    //here we should add the code to search a tree to make sure this city state
+                    //is new. if it is not new, then we simple add the zip to that place.
+                    Place newPlace2 = new Place(cityState1,zipCode1);//create a new place
+                    s.insert(newPlace2);
+                    b.insert(newPlace2);
+                    a.insert(newPlace2);
+                    holdPlace = newPlace2;//sets our temp
+                /*
                 Place newPlace1 = new Place(cityState1, zipCode1);
                 if (data.hasNextLine()) {
                     System.out.println("HERE");// Last line of file check
@@ -108,8 +136,14 @@ public class Trees {
                     b.insert(newPlace1);
                     a.insert(newPlace1);
                 }
-                lastName = cityState.trim();
+
+                 */
+                    //lastName = cityState1;
+                }
             }
+            prevName = cityState1;//readjusts the prevname to what we just looked at so the next line will compare to this line
+           // System.out.println(prevName);
+
         }
     }
 
